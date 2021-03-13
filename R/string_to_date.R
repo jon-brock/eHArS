ad6ad781f9009ebb6a1d9dcf8e2c25a85dd7652a
Jon Brock <jpb2210@cumc.columbia.edu>
2021-03-13 11:17:26 -0500

    embedded function within string_to_date()

--- R/string_to_date.R
+++ R/string_to_date.R
@@ -6,11 +6,21 @@
 #' @export
 string_to_date <- function(x) {
 
-    std_year <- if_else(str_sub(x, start = 1, end = 4) == "", "....", str_sub(x, start = 1, end = 4))
-    std_month <- if_else(str_sub(x, start = 5, end = 6) == "", "..", str_sub(x, start = 5, end = 6))
-    std_day <- if_else(str_sub(x, start = 7, end = 8) == "", "..", str_sub(x, start = 7, end = 8))
+        embedded <- function(x) {
+        std_year <- if_else(str_sub(x, start = 1, end = 4) == "", "....", str_sub(x, start = 1, end = 4))
+        std_month <- if_else(str_sub(x, start = 5, end = 6) == "", "..", str_sub(x, start = 5, end = 6))
+        std_day <- if_else(str_sub(x, start = 7, end = 8) == "", "..", str_sub(x, start = 7, end = 8))
 
-    std_date <- paste0(std_month, '/', std_day, '/', std_year)
+        std_date <- paste0(std_month, '/', std_day, '/', std_year)
+
+    }
+
+    modify_at(x, .at = vars(
+        #cd4_first_stage3_dt,
+        cd4_first_hiv_dt, cd4_low_cnt_dt, cd4_low_pct_dt, cd4_recent_cnt_dt, cd4_recent_cnt_pct_dt,
+        cd4_recent_pct_dt, cd4_vl_first_hiv_dt, vl_first_det_dt, vl_last_non_det_dt, vl_recent_dt,
+        test_recent_dt, antigen_first_pos_dt, dob, enter_dt, hiv_dx_dt, hiv_rep_dt, hiv_aids_dx_dt,
+        hiv_aids_rep_dt, aids_dx_dt, aids_rep_dt, dod), .f = embedded)
 
     return(std_date)
 
