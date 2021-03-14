2c6e9148e2ccc82ce9d2e3da51104701432af704
Jon Brock <jpb2210@cumc.columbia.edu>
2021-03-13 19:50:37 -0500

    reverted changes from test phase.

--- R/string_to_date.R
+++ R/string_to_date.R
@@ -12,13 +12,14 @@ string_to_date <- function(x) {
                 std_month <- if_else(str_sub(x, start = 5, end = 6) == "", "..", str_sub(x, start = 5, end = 6))
                 std_day <- if_else(str_sub(x, start = 7, end = 8) == "", "..", str_sub(x, start = 7, end = 8))
 
-                std_date <- paste0(std_month, '/', std_day, '/', std)
+                std_date <- paste0(std_month, '/', std_day, '/', std_year)
     }
 
-    modify_at(x, .at = any_of(vars(
-        cd4_first_stage3_dt, cd4_first_hiv_dt, cd4_low_cnt_dt, cd4_low_pct_dt, cd4_recent_cnt_dt,
+    modify_at(x, .at = vars(
+        #cd4_first_stage3_dt,
+        cd4_first_hiv_dt, cd4_low_cnt_dt, cd4_low_pct_dt, cd4_recent_cnt_dt,
         cd4_recent_cnt_pct_dt, cd4_recent_pct_dt, cd4_vl_first_hiv_dt, vl_first_det_dt, vl_last_non_det_dt,
         vl_recent_dt, test_recent_dt, antigen_first_pos_dt, dob, enter_dt, hiv_dx_dt, hiv_rep_dt, hiv_aids_dx_dt,
-        hiv_aids_rep_dt, aids_dx_dt, aids_rep_dt, dod)), .f = embed_01)
+        hiv_aids_rep_dt, aids_dx_dt, aids_rep_dt, dod), .f = embed_01)
 
 }
