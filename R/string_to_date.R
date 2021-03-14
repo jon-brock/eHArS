b6d1f59dd69778cd36085a66c72145d0ebe69ad1
Jon Brock <jpb2210@cumc.columbia.edu>
2021-03-13 19:40:50 -0500

    added any_of() to account for missing column.

--- R/string_to_date.R
+++ R/string_to_date.R
@@ -15,11 +15,10 @@ string_to_date <- function(x) {
                 std_date <- paste0(std_month, '/', std_day, '/', std)
     }
 
-    modify_at(x, .at = vars(
-        #cd4_first_stage3_dt,
-        cd4_first_hiv_dt, cd4_low_cnt_dt, cd4_low_pct_dt, cd4_recent_cnt_dt, cd4_recent_cnt_pct_dt,
-        cd4_recent_pct_dt, cd4_vl_first_hiv_dt, vl_first_det_dt, vl_last_non_det_dt, vl_recent_dt,
-        test_recent_dt, antigen_first_pos_dt, dob, enter_dt, hiv_dx_dt, hiv_rep_dt, hiv_aids_dx_dt,
-        hiv_aids_rep_dt, aids_dx_dt, aids_rep_dt, dod), .f = embed_01)
+    modify_at(x, .at = any_of(vars(
+        cd4_first_stage3_dt, cd4_first_hiv_dt, cd4_low_cnt_dt, cd4_low_pct_dt, cd4_recent_cnt_dt,
+        cd4_recent_cnt_pct_dt, cd4_recent_pct_dt, cd4_vl_first_hiv_dt, vl_first_det_dt, vl_last_non_det_dt,
+        vl_recent_dt, test_recent_dt, antigen_first_pos_dt, dob, enter_dt, hiv_dx_dt, hiv_rep_dt, hiv_aids_dx_dt,
+        hiv_aids_rep_dt, aids_dx_dt, aids_rep_dt, dod)), .f = embed_01)
 
 }
